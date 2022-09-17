5a12ab89b08caf4c1db2742c4c89a0f3252f79ca
Anastasia <86nootka36@gmail.com>
2022-09-17 17:49:57 +0300

    culture and facts

---
+++ src/components/Facts.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  name: "Facts"
+}
+</script>
+
+<template>
+  <div class="flex flex-col items-center my-[100px] text-center">
+    <div class="grid grid-cols-5 w-[1300px] mt-[40px] gap-[40px]">
+      <div class="flex flex-col items-center gap-[10px]">
+        <div class="flex items-end gap-[5px]">
+          <div class="text-4xl text-amber-500 font-medium">60+</div>
+          <div class="text-2xl">мест</div>
+        </div>
+        <div>в Москве и Санкт-Петербурге</div>
+      </div>
+      <div class="flex flex-col items-center gap-[10px]">
+        <div class="flex items-end gap-[5px]">
+          <div class="text-4xl text-amber-500 font-medium">8</div>
+          <div class="text-2xl">тысяч</div>
+        </div>
+        <div>чашек кофе делают наши бариста в день</div>
+      </div>
+      <div class="flex flex-col items-center gap-[10px]">
+        <div class="flex items-end gap-[5px]">
+          <div class="text-4xl text-amber-500 font-medium">4,2</div>
+          <div class="text-2xl">тысячи</div>
+        </div>
+        <div>вензелей продано за день</div>
+      </div>
+      <div class="flex flex-col items-center gap-[10px]">
+        <div class="flex items-end gap-[5px]">
+          <div class="text-4xl text-amber-500 font-medium">900</div>
+          <div class="text-2xl">тысяч</div>
+        </div>
+        <div>вензелей с малиной встречаются с нашими гостями в год</div>
+      </div>
+      <div class="flex flex-col items-center gap-[10px]">
+        <div class="flex items-end gap-[5px]">
+          <div class="text-4xl text-amber-500 font-medium">23</div>
+          <div class="text-2xl">года</div>
+        </div>
+        <div>мы радуем нашей продукцией гостей</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
